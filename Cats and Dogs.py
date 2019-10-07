2a966808991bbbaaa05e5506970623da13cf9ebd
hereiam123 <bdemaio1@gmail.com>
2019-10-07 01:37:12 -0400

    Integrating tensorboard to watch how model performing in it's training

--- Cats and Dogs.py
+++ Cats and Dogs.py
@@ -8,6 +8,15 @@ import numpy as np
 import tensorflow.keras
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import TensorBoard
+import time
+
+NAME = "Cats-vs-dog-cnn-64x2-{}".format(int(time.time()))
+log_dir = os.path.join(
+    "logs",
+    NAME
+)
+tensorboard = TensorBoard(log_dir)
 
 DATADIR = "Pets"
 CATEGORIES = ["Dog", "Cat"]
@@ -77,7 +86,8 @@ model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+# model.add(Dense(64))
+# model.add(Activation("relu"))
 
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
@@ -85,4 +95,5 @@ model.add(Activation('sigmoid'))
 model.compile(loss="binary_crossentropy",
               optimizer="adam", metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
+model.fit(X, y, batch_size=32, epochs=20,
+          validation_split=0.3, callbacks=[tensorboard])
