11ed7c3b3c1d00f1a5ab0ae8018367651821c1f2
hereiam123 <bdemaio1@gmail.com>
2019-10-07 00:42:15 -0400

    A little more exploration for setting up model, using a tutorial

--- Cats and Dogs.py
+++ Cats and Dogs.py
@@ -5,6 +5,8 @@ import cv2
 import random
 import pickle
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 
 DATADIR = "Pets"
 CATEGORIES = ["Dog", "Cat"]
@@ -46,6 +48,7 @@ y = []
 for features, label in training_data:
     X.append(features)
     y.append(label)
+    # print(label)
 
 # Saving normalized data to prevent above from rerunning
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
@@ -53,6 +56,32 @@ pickle_out = open("X.pickle", "wb")
 pickle.dump(X, pickle_out)
 pickle_out.close()
 
+y = np.array(y)
 pickle_out = open("y.pickle", "wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()"""
+
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+X = X/255.0
+
+model = Sequential()
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss="binary_crossentropy",
+              optimizer="adam", metrics=['accuracy'])
+
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1)
